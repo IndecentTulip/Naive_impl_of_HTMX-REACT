2c393d6a73574ba5592acf487b20d431f86d741e
IndecentTulip <aleksandrbabenko02@gmail.com>
2025-05-12 21:22:13 -0700

    webcomponents where to much when I was trying to implement this idea of using React + HTMX together in real project, but turns out you can just use 'useRef()' to basically let React Render item and then let you use that DOM node, because if it you can then tell HTMX to process that node, it's possible to brake this, but it's good enough

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,9 @@
 import './Main.css';
-import { useState } from 'react';
+import Demo1 from './Demo1';
+import Demo2 from './Demo2';
 
 const Main = () => {
+<<<<<<< HEAD
   // useState creates a piece of state called "count"
   const [count, setCount] = useState(0);
 
@@ -25,11 +27,16 @@ const Main = () => {
         <demo-3></demo-3>
         <div id="light-dom-output"></div>
 
+=======
+  return (
+      <div className="max-w-xl mx-auto bg-white shadow-lg rounded-lg p-6">
+        <h2 className="text-xl font-semibold mb-4">Elements that contain HTMX</h2>
+        <Demo1 />
+        <Demo2 />
+>>>>>>> 90aaae8 (webcomponents where to much when I was trying to implement this idea of using React + HTMX together in real project, but turns out you can just use 'useRef()' to basically let React Render item and then let you use that DOM node, because if it you can then tell HTMX to process that node, it's possible to brake this, but it's good enough)
       </div>
-    </div>
-
   );
-}
+};
 
 export default Main;
 
--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<<<<<<< HEAD
     <script src="src/webcomponent.js"></script>
     <h1>From here everything is inside of React</h1>
+=======
+>>>>>>> 90aaae8 (webcomponents where to much when I was trying to implement this idea of using React + HTMX together in real project, but turns out you can just use 'useRef()' to basically let React Render item and then let you use that DOM node, because if it you can then tell HTMX to process that node, it's possible to brake this, but it's good enough)
     <App />
   </React.StrictMode>
 );
